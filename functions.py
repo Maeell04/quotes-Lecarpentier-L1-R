9ff9f55f15c1e89f31c861c6e6eb2fc00b594cb7
Tom92i <105479879+Tom92i@users.noreply.github.com>
2023-05-24 18:22:06 +0000

    Définition de la fonction display_quotes

--- functions.py
+++ functions.py
@@ -25,3 +25,12 @@ def add_quote(new_quote, quotes, filename):
     quotes.append(new_quote)
     with open(filename, 'a') as file:
         file.write('\n' + new_quote)
+
+def display_quotes(quotes, count):
+    if count >= len(quotes):
+        print("All Quotes:")
+        view_quotes(quotes)
+    else:
+        print(f"First {count} Quotes:")
+        for i in range(count):
+            print_quote(quotes[i])
